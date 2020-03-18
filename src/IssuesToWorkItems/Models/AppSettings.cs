0a99f83e613cbebe8b7d8adfc5dcb54ab0bcd117
Dan Hellem <dan_hellem@hotmail.com>
2020-03-18 11:19:12 -0700

    Area path to app settings and initial create Fixes #13

--- src/IssuesToWorkItems/Models/AppSettings.cs
+++ src/IssuesToWorkItems/Models/AppSettings.cs
@@ -8,13 +8,14 @@ namespace WebHookReciever.Models
     public class AppSettings
     {
         public string GitHub_Secret { get; set; }
-        public string GitHub_Token { get; set; }
-        public string GitHub_AppName { get; set; }
+        //public string GitHub_Token { get; set; }
+        //public string GitHub_AppName { get; set; }
         public string ADO_Pat { get; set; }
         public string ADO_Org { get; set; }
         public string ADO_Project { get; set; }
         public string ADO_DefaultWIT { get; set; }
         public string ADO_CloseState { get; set; }
-        public string  ADO_NewState { get; set; }
+        public string ADO_NewState { get; set; }
+        public string ADO_AreaPath { get; set; }
     }
 }
